3f18a2cab78f141660271db417cca9a5975efa02
0xloem <0xloem@gmail.com>
2021-01-14 22:23:13 -0500

    remove reference to my source folder from bleak recipe, and clean up a little

--- bleak/backends/p4android/recipes/bleak/__init__.py
+++ bleak/backends/p4android/recipes/bleak/__init__.py
@@ -3,17 +3,15 @@ from pythonforandroid.toolchain import shprint, info
 from pythonforandroid.util import current_directory
 from pythonforandroid import logger
 import sh
-from os.path import join
-import os
+from os.path import join, abspath
 
 
 class BleakRecipe(PythonRecipe):
     version = None
     url = None
     name = "bleak"
-    # site_packages_name = 'bleak'
 
-    src_filename = "/home/user/src/bleak"  #'src'
+    src_filename = join('..', '..', '..', '..', '..')
 
     depends = ["pyjnius"]
     call_hostpython_via_targetpython = False
@@ -27,9 +25,6 @@ class BleakRecipe(PythonRecipe):
             self.get_build_dir(arch),
         )
 
-    def should_build(self, arch):
-        return True  # always rebuild?
-
     def get_recipe_env(self, arch=None, with_flags_in_cc=True):
         env = super().get_recipe_env(arch, with_flags_in_cc)
         # to find jnius and identify p4a
@@ -39,12 +34,9 @@ class BleakRecipe(PythonRecipe):
     def postbuild_arch(self, arch):
         super().postbuild_arch(arch)
 
-        info("Copying java files to dist_dir")
+        info("Copying java files")
 
-        # destdir = join(self.ctx.bootstrap.dist_dir, 'src', 'main', 'java')
-        # destdir = join(self.ctx.bootstrap.distribution.dist_dir, 'src', 'main', 'java')
         destdir = self.ctx.javaclass_dir
-        # os.makedirs(destdir, exist_ok=True)
         path = join(
             self.get_build_dir(arch.arch), "bleak", "backends", "p4android", "java", "."
         )
